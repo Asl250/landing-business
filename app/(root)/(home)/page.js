130297f13d428492ff1bbfefbd8c350efad100cd
ask250 <aslrahidov250@gmail.com>
2024-06-04 18:22:05 +0500

    commit

--- app/(root)/(home)/page.js
+++ app/(root)/(home)/page.js
@@ -7,12 +7,12 @@ import { BookOpenCheck, CalendarCheck, LaptopMinimal, AlarmClockCheck } from 'lu
 
 function Home() {
 	return(
-		<div className={" h-full bg-no-repeat bg-gradient-to-t from-gray-700 to-green-700"}>
-			<div className={'grid md:grid-cols-3 mb-14 pt-5 ml-[15%] max-md:space-y-5 max-md:ml-[25%]'}>
+		<div className={"h-full bg-no-repeat bg-gradient-to-t from-gray-700 to-green-700"}>
+			<div className={'grid md:grid-cols-3 mb-5 pt-5 ml-[15%] max-md:space-y-5 max-md:ml-[25%]'}>
 				<div className={'md:w-[70%] max-md:w-[60%] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 cursor-pointer lg:flex block max-md:flex space-x-3 bg-gradient-to-r from-green-500 to-green-600 rounded-3xl p-5'}>
 					<CalendarCheck className={'w-[70px] h-[70px] max-lg:mx-[25%] max-md:mx-0'} />
 					<div className={'mt-2 max-lg:text-center max-md:text-start'}>
-						<p className={'max-xl:text-xl max-md:text-2xl xl:text-2xl '}>2 may</p>
+						<p className={'max-xl:text-xl max-md:text-2xl xl:text-2xl'}>2 may</p>
 						<p className={'text-sm'}>Boshlanish vaqti</p>
 					</div>
 				</div>
@@ -20,7 +20,7 @@ function Home() {
 					<LaptopMinimal className={'w-[70px] h-[70px] max-lg:mx-[25%] max-md:mx-0'} />
 					<div className={'mt-2'}>
 						<p className={'max-xl:text-xl max-md:text-2xl xl:text-2xl'}>Onlayn</p>
-						<p className={'text-sm'}>O'quv formati</p>
+						<p className={'text-sm'}>Oquv formati</p>
 					</div>
 				</div>
 				<div className={'md:w-[70%] max-md:w-[60%] md: transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 cursor-pointer lg:flex block max-md:flex space-x-3 bg-gradient-to-r from-green-500 to-green-600 rounded-3xl p-5'}>
@@ -43,7 +43,7 @@ function Home() {
 							justo, ultricies nec justo nec, consequat tincidunt mi. Duis sit amet dapibus justo. </p>
 						<Button
 							className={"hidden lg:flex max-md:mx-auto px-20 py-10 rounded-3xl bg-gradient-to-r from-green-400 to-green-500"}>
-							<p className={"text-xl mr-3 "}>Ro'yxatdan O'tish</p>
+							<p className={"text-xl mr-3 "}>Royxatdan Otish</p>
 							<BookOpenCheck className={"w-[60px] h-[40px]"} />
 						</Button>
 					
@@ -52,7 +52,7 @@ function Home() {
 					       className={"lg:mt-[-290px] max-lg:mx-auto max-lg:mt-[-230px] md:w-[450px] max-lg:w-[500px] "} />
 					<Button
 						className={"mt-3 hidden max-lg:flex mx-auto md:px-20 py-10 px-[20%] max-sm:px-[15%] rounded-3xl bg-gradient-to-r from-green-400 to-green-500"}>
-						<p className={"md:text-2xl text-xl mr-3 "}>Ro'yxatdan O'tish</p>
+						<p className={"md:text-2xl text-xl mr-3 "}>Royxatdan Otish</p>
 						<BookOpenCheck className={"md:w-[60px] md:h-[40px] w-[40px] h-[30px]"} />
 					</Button>
 				</div>
